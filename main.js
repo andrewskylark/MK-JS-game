94a791093cef480d1b76d3f0fb230f8ad9ede46c
andrewskylark <andrew.skylark@gmail.com>
2021-10-13 15:30:18 +0400

    task#3

--- main.js
+++ main.js
@@ -1,40 +1,44 @@
 "use strict";
 
 const player1 = {
-    name: 'name',
+    name: 'kitana',
     hp: 100,
-    img: 'src',
+    img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
     weapon: ['1', '2', '3'],
     attack: () => {
         console.log(`${player1.name} Fight`)
     }
 }
-
 const player2 = {
-    name: 'name2',
-    hp: 200,
-    img: 'src',
+    name: 'liukang',
+    hp: 99,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
     weapon: ['1', '2', '3'],
     attack: () => {
         console.log(`${player2.name} Fight`)
     }
 }
 
-const createPlayer = (player, name, hp) => {
+const createPlayer = (player, data) => {
     const $player = document.createElement('div');
     $player.classList.add(player);
+    
     const $progressbar = document.createElement('div');
     $progressbar.classList.add('progressbar');
+    
     const $life = document.createElement('div');
     $life.classList.add('life');
-    $life.innerText= hp
+    $life.innerText= data.hp
+    
     const $name = document.createElement('div');
     $name.classList.add('name');
+    
     const $character = document.createElement('div');
     $character.classList.add('character');
-    $name.innerText = name
+    $name.innerText = data.name
+    
     const $img = document.createElement('img');
-    $img.src = 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif';
+    $img.src = data.img;
 
     const arenas = document.querySelector('.arenas');
 
@@ -46,5 +50,5 @@ const createPlayer = (player, name, hp) => {
     $character.appendChild($img);
 }
 
-createPlayer('player1', 'SCORPION', 50);
-createPlayer('player2', 'SUB-ZERO', 80);
+createPlayer('player1', player1);
+createPlayer('player2', player2);
