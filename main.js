4a9a46a65ee9f4a68124bc9e5ed807670e515407
andrewskylark <andrew.skylark@gmail.com>
2021-10-13 15:19:36 +0400

    task#1 task#2

--- main.js
+++ main.js
@@ -19,3 +19,32 @@ const player2 = {
         console.log(`${player2.name} Fight`)
     }
 }
+
+const createPlayer = (player, name, hp) => {
+    const $player = document.createElement('div');
+    $player.classList.add(player);
+    const $progressbar = document.createElement('div');
+    $progressbar.classList.add('progressbar');
+    const $life = document.createElement('div');
+    $life.classList.add('life');
+    $life.innerText= hp
+    const $name = document.createElement('div');
+    $name.classList.add('name');
+    const $character = document.createElement('div');
+    $character.classList.add('character');
+    $name.innerText = name
+    const $img = document.createElement('img');
+    $img.src = 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif';
+
+    const arenas = document.querySelector('.arenas');
+
+    arenas.appendChild($player);
+    $player.appendChild($progressbar);
+    $progressbar.appendChild($life);
+    $progressbar.appendChild($name);
+    $player.appendChild($character);
+    $character.appendChild($img);
+}
+
+createPlayer('player1', 'SCORPION', 50);
+createPlayer('player2', 'SUB-ZERO', 80);
