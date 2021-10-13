aa80cfa35fc9c68faceb56ae4cb638e7836f6f1e
andrewskylark <andrew.skylark@gmail.com>
2021-10-13 14:47:10 +0400

    task#0

---
+++ main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const player1 = {
+    name: 'name',
+    hp: 100,
+    img: 'src',
+    weapon: ['1', '2', '3'],
+    attack: () => {
+        console.log(`${player1.name} Fight`)
+    }
+}
+
+const player2 = {
+    name: 'name2',
+    hp: 200,
+    img: 'src',
+    weapon: ['1', '2', '3'],
+    attack: () => {
+        console.log(`${player2.name} Fight`)
+    }
+}
